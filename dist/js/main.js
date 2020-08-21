b39dcbac642803edb819abd310a5082b43145ada
Bulat <sadikovba@gmail.com>
2020-08-21 14:02:02 +0300

    Initial

---
+++ dist/js/main.js
@@ -0,0 +1,4 @@
+/*! gulp-start v0.0.1 | (c) 2020 Bulat | MIT License | https://github.com/BulatSa/gulp-start */
+$((function () {
+	
+}));
---
+++ dist/mobile-html/offer_files/bundle.js
@@ -0,0 +1,2 @@
+(function(d,p,t,u){var s=d.createElement(t);s.async=1;s.src=u;p.appendChild(s);})(document, document.body, 'script', '//preregistration.marytrufel.ru/static/js/main.8e40b9cf.js?2c6829c320ac3e81bc6b');
+(function(d,p,t,u){var l=d.createElement(t);l.rel='stylesheet';l.href=u;p.appendChild(l);})(document, document.body, 'link', '//preregistration.marytrufel.ru/static/css/main.b2701a2f.css?2c6829c320ac3e81bc6b');
